0534b38d6e885cc8b796a4d4f4fb5228b6583f18
Matteo Cirami <mcirami@users.noreply.github.com>
2022-03-11 10:58:13 -0600

    updated to breeze

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,8 +13,7 @@
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
         <form method="POST" action="{{ route('login') }}">
-            {{--@csrf--}}
-            <meta name="csrf-token" content="{{ csrf_token() }}">
+            @csrf
             <!-- Email Address -->
             <div>
                 <x-label for="email" :value="__('Email')" />

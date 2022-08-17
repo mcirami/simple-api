3c941a7b02d1c33b8daae8dfcb7f328afb5bf5b2
Matteo Cirami <mcirami@users.noreply.github.com>
2022-08-17 10:38:42 -0500

    fixed dates and table formatting

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,67 +6,69 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <form action="{{ route('export') }}" method="GET" enctype="multipart/form-data">
-                @csrf
-                <div class="row m-5">
-                    <div class="col-6">
-                        <label for="start-date">Start Date</label>
-                        <input name="start-date" id="start-date" class="form-control" type="date">
+        <div class="container">
+            <div class="row">
+                <form class="col-12" action="{{ route('export') }}" method="GET" enctype="multipart/form-data">
+                    @csrf
+                    <div class="row m-5">
+                        <div class="col-6">
+                            <label for="start-date">Start Date</label>
+                            <input name="start-date" id="start-date" class="form-control" type="date">
+                        </div>
+                        <div class="col-6">
+                            <label for="end-date">End Date</label>
+                            <input name="end-date" id="end-date" class="form-control" type="date">
+                        </div>
                     </div>
-                    <div class="col-6">
-                        <label for="end-date">End Date</label>
-                        <input name="end-date" id="end-date" class="form-control" type="date">
-                    </div>
-                </div>
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4">Export Data</button>
-            </form>
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <table class="table table-bordered yajra-datatable">
-                        {{--<form id="filter_form" method="POST">
-                            @csrf
-                            <div class="row m-5">
-                                <div class="col-6">
-                                    <label for="start-date">Start Date</label>
-                                    <input id="start-date" class="form-control" type="date">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4">Export Data</button>
+                </form>
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg col-12">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <table class="table table-bordered yajra-datatable w-100">
+                            {{--<form id="filter_form" method="POST">
+                                @csrf
+                                <div class="row m-5">
+                                    <div class="col-6">
+                                        <label for="start-date">Start Date</label>
+                                        <input id="start-date" class="form-control" type="date">
+                                    </div>
+                                    <div class="col-6">
+                                        <label for="end-date">End Date</label>
+                                        <input id="end-date" class="form-control" type="date">
+                                    </div>
                                 </div>
-                                <div class="col-6">
-                                    <label for="end-date">End Date</label>
-                                    <input id="end-date" class="form-control" type="date">
+                                <div class="row m-5">
+                                    <div class="col-12">
+                                        <button type="submit" id="btn-filter" class="btn btn-primary">Filter</button>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row m-5">
-                                <div class="col-12">
-                                    <button type="submit" id="btn-filter" class="btn btn-primary">Filter</button>
-                                </div>
-                            </div>
-                        </form>--}}
-                        <thead>
-                        <tr>
-                            <th class="w-1/7 text-left p-2 border border-light-blue-500">No.</th>
-                            <th class="w-1/5 text-left p-2 border border-light-blue-500">IP</th>
-                            <th class="w-1/4 text-left p-2 border border-light-blue-500">Email</th>
-                            <th class="w-1/6 text-left p-2 border border-light-blue-500">Source ID</th>
-                            <th class="w-1/5 text-left p-2 border border-light-blue-500">Tracking ID</th>
-                            <th class="w-1/4 text-left p-2 border border-light-blue-500">Timestamp</th>
-                            {{--<th class="w-1/5 text-left p-2 border border-light-blue-500">Action</th>--}}
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {{--@php $count = 0 @endphp
-                        @foreach($data as $dataItem)
-                            <tr @if ($count % 2 != 0) class="bg-blue-100" @endif>
-                                <td class="p-2 border border-light-blue-500">{{$dataItem->ip}}</td>
-                                <td class="p-2 border border-light-blue-500">{{$dataItem->email}}</td>
-                                <td class="p-2 border border-light-blue-500">{{$dataItem->source_id}}</td>
-                                <td class="p-2 border border-light-blue-500">{{$dataItem->tracking_id}}</td>
-                                <td class="p-2 border border-light-blue-500">{{$dataItem->time_stamp}}</td>
+                            </form>--}}
+                            <thead>
+                            <tr>
+                                <th class="w-1/7 text-left p-2 border border-light-blue-500">No.</th>
+                                <th class="w-1/5 text-left p-2 border border-light-blue-500">IP</th>
+                                <th class="w-1/4 text-left p-2 border border-light-blue-500">Email</th>
+                                <th class="w-1/6 text-left p-2 border border-light-blue-500">Source ID</th>
+                                <th class="w-1/5 text-left p-2 border border-light-blue-500">Tracking ID</th>
+                                <th class="w-1/4 text-left p-2 border border-light-blue-500">Timestamp</th>
+                                {{--<th class="w-1/5 text-left p-2 border border-light-blue-500">Action</th>--}}
                             </tr>
-                            @php ++$count @endphp
-                        @endforeach--}}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                            {{--@php $count = 0 @endphp
+                            @foreach($data as $dataItem)
+                                <tr @if ($count % 2 != 0) class="bg-blue-100" @endif>
+                                    <td class="p-2 border border-light-blue-500">{{$dataItem->ip}}</td>
+                                    <td class="p-2 border border-light-blue-500">{{$dataItem->email}}</td>
+                                    <td class="p-2 border border-light-blue-500">{{$dataItem->source_id}}</td>
+                                    <td class="p-2 border border-light-blue-500">{{$dataItem->tracking_id}}</td>
+                                    <td class="p-2 border border-light-blue-500">{{$dataItem->time_stamp}}</td>
+                                </tr>
+                                @php ++$count @endphp
+                            @endforeach--}}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
